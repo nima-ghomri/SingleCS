3ddcc08c9f96074a44d99b2ecf391b57bff081f9
Nima Ghomri <nima.ghomri@gmail.com>
2021-11-14 02:33:54 +0330

    Initial Commit

--- SingleCS/Program.cs
+++ SingleCS/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace SingleCS
 {
@@ -6,7 +7,7 @@ namespace SingleCS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
