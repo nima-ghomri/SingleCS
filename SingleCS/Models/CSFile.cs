977b33761988bafed64118138ffd2889ffb7826f
Nima Ghomri <nima.ghomri@gmail.com>
2022-01-22 23:13:28 +0330

    Add CSFile

---
+++ SingleCS/Models/CSFile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SingleCS.Models
+{
+    public class CSFile
+    {
+        public List<string> Usings { get; }
+        public string Body { get; }
+
+
+        public CSFile(string path)
+        {
+            var content = File.ReadAllText(path);
+            Usings = Regex.Matches(content, @"using(.*);").Select(g => g.Value).ToList();
+            Body = Regex.Match(content, @"name[^$]*$").Value;
+        }
+    }
+}
