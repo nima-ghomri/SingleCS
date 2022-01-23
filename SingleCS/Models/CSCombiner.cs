cea6a282f634cf0457390244ae45ae0b537ca0f5
Nima Ghomri <nima.ghomri@gmail.com>
2022-01-24 00:05:09 +0330

    Create combiner

---
+++ SingleCS/Models/CSCombiner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingleCS.Models
+{
+    public class CSCombiner : ICSCombiner
+    {
+        public ICSFile Combine(ICSFile file1, ICSFile file2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SingleCS/Models/ICSCombiner.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingleCS.Models
+{
+    public interface ICSCombiner
+    {
+        ICSFile Combine(ICSFile file1, ICSFile file2);
+    }
+}
