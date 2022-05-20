edcbc447c8782434dd3fc445a82634b3724073b3
Nima Ghomri <nima.ghomri@gmail.com>
2022-05-20 20:01:04 +0430

    Add FindAllRelativeAndAbsolute method

--- SingleCS/Globber.cs
+++ SingleCS/Globber.cs
@@ -14,25 +14,29 @@ namespace SingleCS.Models
 
         public Globber(IEnumerable<string> include, IEnumerable<string> exclude, IEnumerable<string> mains, string directory)
         {
-            filesPath = include.GroupBy(path => Path.IsPathFullyQualified(path) ? path : null).SelectMany((paths) =>
-               {
-                   var absolute = paths.Key != null;
-                   var current = absolute ? Path.GetPathRoot(paths.First()) : directory;
-                   var patterns = absolute ? new[] { Path.GetRelativePath(current, paths.First()) } : paths.ToArray();
-                   var matcher = new Matcher();
-                   matcher.AddIncludePatterns(patterns);
-                   matcher.AddExcludePatterns(exclude);
-                   return matcher.GetResultsInFullPath(current);
-               });
+            filesPath = FindAllRelativeAndAbsolute(include, exclude, directory);
 
             if (filesPath.Count() < 1)
                 throw new InvalidOperationException("Not found any files.");
 
-            var matcher = new Matcher();
-            matcher.AddIncludePatterns(mains);
-            mainsPath = matcher.GetResultsInFullPath(directory);
+            mainsPath = FindAllRelativeAndAbsolute(mains, null, directory);
             if (mainsPath.Count() < 1)
                 mainsPath = new[] { filesPath.First() };
+        }
+
+        private IEnumerable<string> FindAllRelativeAndAbsolute(IEnumerable<string> include, IEnumerable<string> exclude = null, string directory = null)
+        {
+            return include.GroupBy(path => Path.IsPathFullyQualified(path) ? path : null).SelectMany((paths) =>
+             {
+                 var absolute = paths.Key != null;
+                 var current = absolute ? Path.GetPathRoot(paths.First()) : directory ?? Directory.GetCurrentDirectory();
+                 var patterns = absolute ? new[] { Path.GetRelativePath(current, paths.First()) } : paths.ToArray();
+                 var matcher = new Matcher();
+                 matcher.AddIncludePatterns(patterns);
+                 if (exclude != null)
+                     matcher.AddExcludePatterns(exclude);
+                 return matcher.GetResultsInFullPath(current);
+             });
         }
 
         public IEnumerable<IEnumerable<ICSFile>> GetFileSets(Func<string, ICSFile> creator)
